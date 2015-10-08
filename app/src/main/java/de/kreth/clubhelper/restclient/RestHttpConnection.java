a1294f9e127810b12fe492f71cb8493c750756ce
Markus Kreth <markus.kreth@web.de>
2015-10-08 23:35:34 +0200

    RestHttpConnection tested.

--- app/src/main/java/de/kreth/clubhelper/restclient/RestHttpConnection.java
+++ app/src/main/java/de/kreth/clubhelper/restclient/RestHttpConnection.java
@@ -5,6 +5,7 @@ import com.google.gson.GsonBuilder;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.HttpURLConnection;
@@ -13,6 +14,7 @@ import java.net.URL;
 import de.kreth.clubhelper.Data;
 
 /**
+ * Connects to Server and sends Data objects.
  * Created by markus on 02.09.15.
  */
 public class RestHttpConnection {
@@ -51,25 +53,28 @@ public class RestHttpConnection {
 
     private HttpURLConnection getConnection() throws IOException {
 
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        HttpURLConnection con = getConnection(url);
 
-        //add request header
         con.setRequestMethod(httpRequestType);
         con.setRequestProperty("User-Agent", USER_AGENT);
         con.setRequestProperty("Content-Type", "application/json");
         con.setRequestProperty("Accept", "application/json");
-        if(httpRequestType != HTTP_REQUEST_GET)
+        if (!httpRequestType.equals(HTTP_REQUEST_GET))
             con.setDoOutput(true);
 
         return con;
     }
 
+    protected HttpURLConnection getConnection(URL url) throws IOException {
+        return (HttpURLConnection) url.openConnection();
+    }
+
     public void send(String json) throws IOException {
-        if(con != null)
+        if (con != null)
             throw new IllegalStateException("Connection was used before");
 
         con = getConnection();
-        if(httpRequestType != HTTP_REQUEST_GET) {
+        if (!httpRequestType.equals(HTTP_REQUEST_GET)) {
             PrintStream str = new PrintStream(con.getOutputStream());
             str.print(json);
             str.flush();
@@ -78,11 +83,16 @@ public class RestHttpConnection {
         responseCode = con.getResponseCode();
     }
 
+    @SuppressWarnings("unchecked")
     public <T extends Data> T send(T obj) throws IOException {
-        T data = null;
+
         send(gson.toJson(obj));
-        if(getResponseCode() == HttpURLConnection.HTTP_OK)
+
+        T data = null;
+        if (getResponseCode() == HttpURLConnection.HTTP_OK) {
             data = (T) gson.fromJson(getResponse(), obj.getClass());
+        }
+
         return data;
     }
 
@@ -92,11 +102,12 @@ public class RestHttpConnection {
 
     public String getResponse() throws IOException {
 
-        if(this.response == null) {
+        if (this.response == null) {
+            final InputStream inputStream = con.getInputStream();
             BufferedReader in = new BufferedReader(
-                    new InputStreamReader(con.getInputStream()));
+                    new InputStreamReader(inputStream));
             String inputLine;
-            StringBuffer response = new StringBuffer();
+            StringBuilder response = new StringBuilder();
 
             while ((inputLine = in.readLine()) != null) {
                 response.append(inputLine);
@@ -113,7 +124,7 @@ public class RestHttpConnection {
     }
 
     public String getRequestMethod() {
-        if(con != null)
+        if (con != null)
             return con.getRequestMethod();
         else
             return httpRequestType;
---
+++ app/src/test/java/de/kreth/clubhelper/restclient/RestHttpConnectionTest.java
@@ -0,0 +1,132 @@
+package de.kreth.clubhelper.restclient;
+
+import android.test.suitebuilder.annotation.MediumTest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import de.kreth.clubhelper.Person;
+import de.kreth.clubhelper.PersonType;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@MediumTest
+@RunWith(MockitoJUnitRunner.class)
+public class RestHttpConnectionTest {
+
+    @Mock
+    private HttpURLConnection connection;
+    private ByteArrayOutputStream outputStream;
+    private JsonMapper<Person> mapper;
+
+    private Calendar created;
+    private Calendar birth;
+    private Calendar changed;
+    private Person data;
+    private URL url;
+
+    @Before
+    public void setup() throws IOException {
+        MockitoAnnotations.initMocks(this);
+        outputStream = new ByteArrayOutputStream();
+        mapper = new JsonMapper<>();
+
+        when(connection.getOutputStream()).thenReturn(outputStream);
+
+        created = new GregorianCalendar(2015, Calendar.JANUARY, 1, 8, 0, 0);
+        birth = new GregorianCalendar(1973, Calendar.AUGUST, 21, 8, 0, 0);
+        changed = (Calendar) created.clone();
+        changed.add(Calendar.HOUR_OF_DAY, 2);
+        data = new Person(-1L, "Markus", "Kreth", PersonType.STAFF.name(), birth.getTime(), changed.getTime(), created.getTime());
+        url = new URL("http://localhost");
+    }
+
+    @Test
+    public void testSendPerson() throws IOException {
+
+        when(connection.getResponseCode()).thenReturn(HttpURLConnection.HTTP_OK);
+
+        RestHttpConnection restHttpConnection = new MockRestHttpConnection(url, RestHttpConnection.HTTP_REQUEST_GET);
+
+        Person src = new Person(1L, "Markus", "Kreth", PersonType.STAFF.name(), birth.getTime(), changed.getTime(), created.getTime());
+        String srcJson = mapper.toJson(src);
+
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(srcJson.getBytes());
+        when(connection.getInputStream()).thenReturn(inputStream);
+        Person result = restHttpConnection.send(data);
+
+        verify(connection).setRequestMethod(RestHttpConnection.HTTP_REQUEST_GET);
+        verify(connection).setRequestProperty("User-Agent", "Mozilla/5.0");
+        verify(connection).setRequestProperty("Content-Type", "application/json");
+        verify(connection).setRequestProperty("Accept", "application/json");
+
+    }
+
+    @Test
+    public void testSendPostPerson() throws IOException {
+
+        when(connection.getResponseCode()).thenReturn(HttpURLConnection.HTTP_OK);
+
+        RestHttpConnection restHttpConnection = new MockRestHttpConnection(url, RestHttpConnection.HTTP_REQUEST_POST);
+
+        Person src = new Person(1L, "Markus", "Kreth", PersonType.STAFF.name(), birth.getTime(), changed.getTime(), created.getTime());
+        String srcJson = mapper.toJson(src);
+
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(srcJson.getBytes());
+        when(connection.getInputStream()).thenReturn(inputStream);
+        Person result = restHttpConnection.send(data);
+
+        verify(connection).setRequestMethod(RestHttpConnection.HTTP_REQUEST_POST);
+        verify(connection).setRequestProperty("User-Agent", "Mozilla/5.0");
+        verify(connection).setRequestProperty("Content-Type", "application/json");
+        verify(connection).setRequestProperty("Accept", "application/json");
+        verify(connection).setDoOutput(true);
+
+        assertEquals(src.getId(), result.getId());
+        assertEquals(src.getBirth(), result.getBirth());
+        assertEquals(src.getChanged(), result.getChanged());
+        assertEquals(src.getCreated(), result.getCreated());
+        assertEquals(src.getPersonType(), result.getPersonType());
+        assertEquals(src.getPrename(), result.getPrename());
+        assertEquals(src.getSurname(), result.getSurname());
+        assertEquals(src.getType(), result.getType());
+
+        String json = outputStream.toString();
+        final Person sendPerson = mapper.fromJson(json, Person.class);
+
+        assertEquals(-1L, sendPerson.getId().longValue());
+        assertEquals(src.getBirth(), sendPerson.getBirth());
+        assertEquals(src.getChanged(), sendPerson.getChanged());
+        assertEquals(src.getCreated(), sendPerson.getCreated());
+        assertEquals(src.getPersonType(), sendPerson.getPersonType());
+        assertEquals(src.getPrename(), sendPerson.getPrename());
+        assertEquals(src.getSurname(), sendPerson.getSurname());
+        assertEquals(src.getType(), sendPerson.getType());
+    }
+
+    private class MockRestHttpConnection extends RestHttpConnection {
+
+        public MockRestHttpConnection(URL url, String httpRequestType) {
+            super(url, httpRequestType);
+        }
+
+        @Override
+        protected HttpURLConnection getConnection(URL url) throws IOException {
+            return connection;
+        }
+    }
+}
